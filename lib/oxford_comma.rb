877807be7db3f3747f53d92e28fdaf43a9da90c4
Taylor Booth <taylorkbooth@gmail.com>
2020-09-11 23:34:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,9 @@ def oxford_comma(array)
   oxford_comma = ["kiwi"].join
 end
 
-
+def oxford_comma(array)
+  oxford_comma = ["kiwi", "durian"].join
+end
 
 
 # ["kiwi","durian","starfruit","mangos","dragon fruits"]
