5f524bcc606a02a361e6d7c126faccfc3efb1ddb
Taylor Booth <taylorkbooth@gmail.com>
2020-09-11 23:39:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,13 @@
-def oxford_comma(array)
-  oxford_comma = ["kiwi"].join
-end
+# def oxford_comma(array)
+#   oxford_comma = ["kiwi"].join
+# end
 
-def oxford_comma1(array)
-  oxford_comma1 = ["kiwi", "durian"].join(" and ")
+# def oxford_comma(array)
+#   oxford_comma1 = ["kiwi", "durian"].join(" and ")
+# end
+
+def oxford_comma(array)
+  oxford_comma = ["kiwi", "durian", "starfruit"].join(" and ")
 end
 
 
