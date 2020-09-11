8b99c9d41db8f4f5a2b847e588c6326ae6cd6fee
Taylor Booth <taylorkbooth@gmail.com>
2020-09-11 23:53:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     return array.join(" and ")
     elsif array.size >= 3
     arr = array.pop
-    return array.join(", ") + "and" + arr 
+    return array.join(", ") + "to" + arr 
   end
 end
 
