b8b670a0a72b4a249be0d11bbf8e005ef5aa2eed
Taylor Booth <taylorkbooth@gmail.com>
2020-09-11 23:50:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,6 @@
-# def oxford_comma(array)
-#   oxford_comma = ["kiwi"].join
-# end
-
-# def oxford_comma(array)
-#   oxford_comma1 = ["kiwi", "durian"].join(" and ")
-# end
-
 def oxford_comma(array)
-  oxford_comma = ["kiwi", "durian", "starfruit"].join(", ")<<(", and")
+  oxford_comma = ["kiwi", "durian"].join(" and ")
+end
 end
 
 
