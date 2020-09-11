fd9043e88940d9a31af8880ff911ba6a37b5d229
Taylor Booth <taylorkbooth@gmail.com>
2020-09-11 23:55:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
     if array.size == 1
     return array[0]
     elsif array.size == 2 
-    return array.join(" and ")
+    return array.join(", and")
     elsif array.size >= 3
     arr = array.pop
     return array.join(", ") + ", and" + arr 
