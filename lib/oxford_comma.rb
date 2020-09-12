f885d16675f9b1509f6310b2217a425c212ddb2f
Taylor Booth <taylorkbooth@gmail.com>
2020-09-12 00:06:42 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.size == 1
+    return array[0]
+    elsif array.size == 2 
+    return array.join(" and ")
+    elsif array.size >= 3
+    arr = array.pop
+    return array.join(", ") + ", and " + arr 
+  end
 end
